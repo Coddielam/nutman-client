84d665d666c318e36e4027851dc0439d9f7f2feb
Eddie Lam <eddie.cm.lam@gtomato.com>
2022-09-30 11:05:57 +0800

    init checkout page

---
+++ pages/checkout.tsx
@@ -0,0 +1,18 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+
+const Checkout: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>Nutman - Checkout</title>
+      </Head>
+
+      <main>
+        Check me out
+      </main>
+    </div>
+  )
+}
+
+export default Checkout
