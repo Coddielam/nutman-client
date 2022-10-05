699f17925acba6874736ed9d23b7b2648bec5487
Eddie Lam <eddie.cm.lam@gtomato.com>
2022-10-05 15:15:46 +0800

    feat: stripe webhook setup

---
+++ pages/api/webhooks/index.ts
@@ -0,0 +1,65 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import { buffer } from 'stream/consumers';
+import Cors from 'micro-cors'
+
+import Stripe from 'stripe'
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
+  apiVersion: '2022-08-01',
+})
+
+const webhookSecret: string = process.env.STRIPE_WEBHOOK_SECRET!;
+
+// Stripe requires the raw body to construct the event
+export const config = {
+  api: {
+    bodyParser: false,
+  }
+}
+
+// allowing post this api to receive post requests
+const cors = Cors({
+  allowMethods: ['POST', 'HEAD']
+});
+
+
+// verifying requests come from stripe
+const handleStripeWebhooks = async (req: NextApiRequest, res: NextApiResponse) => {
+
+  if (req.method === 'POST') {
+    const buf = await buffer(req);
+    const sig = req.headers['stripe-signature']!;
+
+    let event: Stripe.Event;
+
+    try {
+      event = stripe.webhooks.constructEvent(buf.toString(), sig, webhookSecret)
+    } catch (err) {
+      const errorMessage = err instanceof Error ? err.message : 'Unknown error'
+      // On error, log and return the error message.
+      if (err! instanceof Error) console.log(err)
+      console.log(`❌ Error message: ${errorMessage}`)
+      res.status(400).send(`Webhook Error: ${errorMessage}`)
+      return
+    }
+  
+    // Handle the event
+    switch (event.type) {
+      case 'payment_intent.succeeded':
+        const paymentIntent = event.data.object;
+        // Then define and call a function to handle the event payment_intent.succeeded
+        break;
+      // ... handle other event types
+      default:
+        console.log(`Unhandled event type ${event.type}`);
+    }
+  
+    // Return a 200 response to acknowledge receipt of the event
+    res.json({ received: true })
+  } else {
+    res.setHeader('Allow', 'POST');
+    res.status(405).end('Method Not Allowed')
+  }
+
+}
+
+export default cors(handleStripeWebhooks as any);
