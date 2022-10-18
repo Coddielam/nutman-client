cc84a59f5fdd0debb2f5dfece0cd3e5c3755d7b1
Eddie Lam <eddie.cm.lam@gtomato.com>
2022-10-18 12:16:19 +0800

    feat: google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,16 +4,32 @@ import Navbar from '@components/Navbar';
 import CartContextWrapper from '@root/context/cart';
 import Footer from '@components/Footer';
 import { appWithTranslation } from 'next-i18next';
+import Script from 'next/script';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <div className="max-w-md mx-auto h-vh relative transition-all duration-200">
-      <CartContextWrapper>
-        <Navbar />
-        <Component {...pageProps} />
-      </CartContextWrapper>
-      <Footer />
-    </div>
+    <>
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-3EKJ4KL0EP"
+        strategy="afterInteractive"
+      />
+
+      <Script strategy="afterInteractive" id="google-analytics">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-3EKJ4KL0EP');`}
+      </Script>
+
+      <div className="max-w-md mx-auto h-vh relative transition-all duration-200">
+        <CartContextWrapper>
+          <Navbar />
+          <Component {...pageProps} />
+        </CartContextWrapper>
+        <Footer />
+      </div>
+    </>
   );
 }
 
